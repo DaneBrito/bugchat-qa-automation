60f8af358c9bbacc7913be6dd06c30d0d6f15031
Daniele Brito <103685009+DaneBrito@users.noreply.github.com>
2025-11-06 16:50:00 -0300

    Add API interception for users endpoint in tests

--- cypress/e2e/api/users.cy.js
+++ cypress/e2e/api/users.cy.js
@@ -1,9 +1,19 @@
 /// <reference types="cypress" />
 describe('Users API shape (sample)', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/users', {
+      statusCode: 200,
+      body: { users: [{ id: 1, name: 'QA Dani' }] },
+    }).as('getUsers')
+  })
+
   it('GET returns metadata (sample placeholder)', () => {
-    cy.request('https://httpbin.org/anything/users').then((res) => {
-      expect(res.status).to.eq(200)
-      expect(res.body).to.have.property('method', 'GET')
-    })
+    cy.request('/api/users')
+      .its('body.users')
+      .should((arr) => {
+        expect(arr).to.be.an('array')
+        expect(arr[0]).to.have.property('name')
+      })
+    cy.wait('@getUsers')
   })
 })
